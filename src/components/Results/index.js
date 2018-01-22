8b83b265efc99d51215a08b58b92e54686493245
sevgit <esportsmarketer@gmail.com>
2018-01-22 16:56:01 -0300

    layed out the basic structure for our scoreCard component

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import './index.css'
 
 
+let score = {
+  total: 0,
+  result: ""
+}
+function scoreCard(q, a) {
+  let scoreCard = 0;
+  
+ a.map((value,index) => {
+  if(q[index].correctas === value) {
+    scoreCard = scoreCard +1;
+  }
+}) 
+  score.total = scoreCard;
+  score.result = scoreCard >= 25 ? "APROBADO" : "REPROBADO" ;
+}
+
 const Header = (props) => {
-    let correctAnswers = 0;
+  scoreCard(props.questionnaire, props.answersArray)
     return (
-     
+      
       <div className='results'>
-     
+      <h2>Resultado: {score.result} </h2>
+      <p>Correctas: {score.total}/30</p>
       <ul>
           {props.answersArray.map((value, index) => 
                     /**/
@@ -19,8 +36,7 @@ const Header = (props) => {
                      <span className="results__correctAnswer">{props.questionnaire[index].respuestas[props.questionnaire[index].correctas]}</span>
                      </li>)}
       </ul>
-        
-        
+     
         
       </div>
 
