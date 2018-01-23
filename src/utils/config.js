9b71aa6c859bcf670c85bd3ed7b0d1499f1a69fe
sevgit <esportsmarketer@gmail.com>
2018-01-23 15:31:11 -0300

    abstracted some config values

---
+++ src/utils/config.js
@@ -0,0 +1,17 @@
+export const testConfig = {
+    //The amount of responses required before showing the results.
+    requiredAnswers: 30,
+    //The minimum score to pass the test.
+    minimumScore: 25,
+    //firebase config
+   
+    
+}
+export const firebaseConfig = {
+    authDomain: "academia-lomas.firebaseapp.com",
+    apiKey: "AIzaSyDBZolyXfLHcyc24j_rUsPJigvXMmcnnLM",
+    databaseURL: "https://academia-lomas.firebaseio.com",
+    projectId: "academia-lomas",
+    storageBucket: "academia-lomas.appspot.com",
+    messagingSenderId: "537654016832"
+}
